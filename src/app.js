2fcdbf6390acc09719dbd3f98a862af51520317c
Owais <r.owaisakram@gmail.com>
2024-07-05 12:02:17 +0500

    User Auth implemented

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ import "dotenv/config";
 import express from "express";
 import { connectDB } from "./db/config.js";
 import syncDB from "./db/init.js";
-
+import allRouters from "./routes/index.js";
 connectDB();
 syncDB().then(() => {
   console.log("DB data synced");
 });
+
 const app = express();
 app.use(express.json());
+app.use(allRouters);
 app.listen(3000, () => {
   console.log("server started at port:3000");
 });
